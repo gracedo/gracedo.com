9286517a99fa16c2db9827309f79d1b21e21bc70
Grace Do <xgrace@gmail.com>
2014-04-10 00:57:36 -0700

    jquery scrolling effects

--- app/assets/javascripts/gracedo_com.js
+++ app/assets/javascripts/gracedo_com.js
@@ -7,6 +7,16 @@ window.GracedoCom = {
   }
 };
 
+var animateScroll = function() {
+  $('.navlink').click(function(event) {
+    event.preventDefault();
+    var navbarHeight = 200;
+    var target = $(event.target).text();
+    $('body, html').animate({scrollTop: ($('#'+target).offset().top - navbarHeight)}, 500);
+  })
+}
+
 $(document).ready(function(){
   GracedoCom.initialize();
+  animateScroll();
 });
