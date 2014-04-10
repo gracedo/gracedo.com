62ae0a534a8e05487647b7e631cee992908a896c
Grace Do <xgrace@gmail.com>
2014-04-10 01:40:34 -0700

    remove empty backbone dirs so heroku compiles

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -17,9 +17,9 @@
 //= require underscore
 //= require backbone
 //= require gracedo_com
-//= require_tree ../templates
-//= require_tree ./models
-//= require_tree ./collections
-//= require_tree ./views
-//= require_tree ./routers
+
+
+
+
+
 //= require_tree .
